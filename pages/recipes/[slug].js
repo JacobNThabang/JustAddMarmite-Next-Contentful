f79af9cbeef7cba18140026e457579e01690e7bb
jacobtouchf <jacob@touchfoundry.co.za>
2023-03-21 23:08:19 +0200

    redirect

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -28,6 +28,15 @@ export const getStaticPaths = async () => {
 export async function getStaticProps({ params }) {
     const { items } =  await client.getEntries({ content_type: 'recipe', 'fields.slug': params.slug });
 
+    if (!items.length) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false
+            }
+        }
+    }
+
     return {
         props: {
             recipe: items[0]
